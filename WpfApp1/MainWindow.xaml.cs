5e8570bee2d132b1c47ccfcb3add56c073ac2f09
okosvagyok <kemenes.marton@blathy.info>
2024-02-14 10:33:42 +0100

    hajoberles v0.1

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,31 +18,45 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<hajo>hajok = new List<hajo>();
         public MainWindow()
         {
             InitializeComponent();
+            //beolvasás
         }
-
-        int szamCount = 0;
-        string muveletEk = "";
-        private void szam(object sender, RoutedEventArgs e)
+        class hajo()
         {
-            if (szamCount == 0)
+            public int sorszam;
+            public string nev;
+            public string tipus;
+            public int maxutasszam;
+            public int berletdij;
+
+            public hajo(int sorszam, string nev, string tipus, int maxutasszam, int berletdij)
             {
-                szamCount++;
-                muveletEk += szamCount.ToString();
-                kiir.Content += ((Button)sender).Content.ToString();
+                this.sorszam = sorszam;
+                this.nev = nev;
+                this.tipus = tipus;
+                this.maxutasszam = maxutasszam;
+                this.berletdij = berletdij;
             }
         }
-
-        private void muvelet(object sender, RoutedEventArgs e)
+        void beolvasas()
         {
-            MessageBox.Show("2");
+            StreamReader f = new StreamReader(@"C:\Users\kemenes.marton\Downloads\Sétahajó_WPF\Sétahajó_WPF\hajo-1.txt");
+            f.ReadLine();
+            while (!f.EndOfStream)
+            {
+                string[] darabolt = f.ReadLine().Split('\t');
+                hajok.Add(new hajo(int.Parse(darabolt[0]), darabolt[1], darabolt[2], int.Parse(darabolt[3]), int.Parse(darabolt[4])));
+            }
+            f.Close();
+
         }
 
-        private void egyenlo(object sender, RoutedEventArgs e)
+        private void Keres()
         {
-            MessageBox.Show("0");
+
         }
     }
 }
