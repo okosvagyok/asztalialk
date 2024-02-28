55f58257ef082d5d85bd59e15abab89a7ab78cc5
okosvagyok <kemenes.marton@blathy.info>
2024-02-28 10:34:20 +0100

    pizzaWebShop (v1.0)

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -22,5 +22,113 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+        public string pizza = "";
+        public string topping1 = "";
+        public string topping2 = "";
+        public string topping3 = "";
+        public int margheritaPrice = 3199;
+        public int romanaPrice = 3499;
+        public int napoletanaPrice = 3499;
+        public int cheesePrice = 400;
+        public int hamPrice = 300;
+        public int baconPrice = 800;
+        public int pizzaPrice = 0;
+        public int toppingPrice = 0;
+        public string toppings = "";
+        private void orderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            pizzaPrice = 0;
+            toppingPrice = 0;
+            toppings = "";
+            topping1 = "";
+            topping2 = "";
+            topping3 = "";
+            if (margherita.IsChecked == true)
+            {
+                pizza = "Margherita";
+                pizzaPrice += margheritaPrice;
+                if (extraCheese.IsChecked == true)
+                {
+                    topping1 = "sajt";
+                    toppingPrice += cheesePrice;
+                }
+                if (extraHam.IsChecked == true)
+                {
+                    topping2 = "sonka";
+                    toppingPrice += hamPrice;
+                }
+                if (extraBacon.IsChecked == true)
+                {
+                    topping3 = "bacon";
+                    toppingPrice += baconPrice;
+                }
+            }
+            else if (romana.IsChecked == true)
+            {
+                pizza = "Romana";
+                pizzaPrice += romanaPrice;
+                if (extraCheese.IsChecked == true)
+                {
+                    topping1 = "sajt";
+                    toppingPrice += cheesePrice;
+                }
+                if (extraHam.IsChecked == true)
+                {
+                    topping2 = "sonka";
+                    toppingPrice += hamPrice;
+                }
+                if (extraBacon.IsChecked == true)
+                {
+                    topping3 = "bacon";
+                    toppingPrice += baconPrice;
+                }
+            }
+            else if (napoletana.IsChecked == true)
+            {
+                pizza = "Napoletana";
+                pizzaPrice += napoletanaPrice;
+                if (extraCheese.IsChecked == true)
+                {
+                    topping1 = "sajt";
+                    toppingPrice += cheesePrice;
+                }
+                if (extraHam.IsChecked == true)
+                {
+                    topping2 = "sonka";
+                    toppingPrice += hamPrice;
+                }
+                if (extraBacon.IsChecked == true)
+                {
+                    topping3 = "bacon";
+                    toppingPrice += baconPrice;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Válassz ki egy pizzát!");
+            }
+            if (deliveryName.Text == "" || deliveryPhone.Text == "" || deliveryAdress.Text == "")
+            {
+                MessageBox.Show("Kérlek, add meg az adataidat.");
+            }
+            else
+            {
+                if (topping1 == "")
+                {
+                    toppings = $"{topping2}, {topping3}";
+                }else if (topping2 == "")
+                {
+                    toppings = $"{topping1}, {topping3}";
+                }else if (topping3 == "")
+                {
+                    toppings = $"{topping1}, {topping2}";
+                }
+                else
+                {
+                    toppings = $"{topping1}, {topping2}, {topping3}";
+                }
+                deliveryInfos.Content = $"Sikeresen megrendelted a {pizza} pizzádat. Extra feltétek: {toppings}.\nA végösszeg: {pizzaPrice+toppingPrice} Ft.";
+            }
+        }
     }
 };
