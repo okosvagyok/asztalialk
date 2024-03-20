7dbfbf0d68f78557bbc5690f63683b784a8519de
okosvagyok <kemenes.marton@blathy.info>
2024-03-20 11:33:48 +0100

    Oscars (v0.9)

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace WpfApp1
         }
         Random rnd = new Random();
         List<Films> films = new List<Films>();
+        List<string> titles = new List<string>();
         public void read()
         {
             StreamReader f = new StreamReader("C:\\Users\\kemenes.marton\\Downloads\\Oscar\\Oscar\\oscar.csv");
@@ -36,19 +37,45 @@ namespace WpfApp1
                 string sor = f.ReadLine();
                 films.Add(new Films(sor.Split(";")[0], sor.Split(";")[1], int.Parse(sor.Split(";")[2]), int.Parse(sor.Split(";")[3]), int.Parse(sor.Split(";")[4])));
             }
+            for(int i = 0; i < films.Count; i++)
+            {
+                titles.Add(films[i].title);
+            }
+            titles.Sort();
         }
         public void outTitles()
         {
             for (int i = 0; i < films.Count(); i++)
             {
-                oscarsFilms.Items.Add(films[i].title);
+                oscarsFilms.Items.Add(titles[i]);
             }
         }
         private void newFilm_Click(object sender, RoutedEventArgs e)
         {
-            if (newTitle.Text == "" || newYear.Text == "" || newNominees.Text == "" || newAwards.Text == "") { MessageBox.Show("Kérlek, tölts ki minden mezőt!"); };
-            oscarsFilms.Items.Add(newTitle.Text);
-            films.Add(new Films(rnd.Next(1, 10).ToString(), newTitle.Text, int.Parse(newYear.Text), int.Parse(newAwards.Text), int.Parse(newNominees.Text)));
+            int n = 0;
+            if (newTitle.Text.Length == 0 || newYear.Text.Length == 0 || newNominees.Text.Length == 0 || newAwards.Text.Length == 0)
+            {
+                MessageBox.Show("Kérlek, tölts ki minden mezőt!");
+            }
+            else if (!int.TryParse(newNominees.Text, out n) || !int.TryParse(newAwards.Text, out n) || !int.TryParse(newYear.Text, out n))
+            {
+                MessageBox.Show("Kérlek, számot adj meg!");
+            }
+            else if (int.TryParse(newNominees.Text, out n) || int.TryParse(newAwards.Text, out n))
+            {
+                int nominees = int.Parse(newNominees.Text);
+                int awards = int.Parse(newAwards.Text);
+                if (nominees < awards)
+                {
+                    MessageBox.Show("A film nem kaphatott több díjat, mint jelölést!");
+                }
+            }
+            else //if (int.TryParse(newNominees.Text, out n) || int.TryParse(newAwards.Text, out n))
+            {
+                oscarsFilms.Items.Add(newTitle.Text);
+                films.Add(new Films(rnd.Next(1, 10).ToString(), newTitle.Text, int.Parse(newYear.Text), int.Parse(newAwards.Text), int.Parse(newNominees.Text)));
+                MessageBox.Show("Sikeresen hozzáadtad a filmet a listához");
+            }
         }
         private void mostAwards_Click(object sender, RoutedEventArgs e)
         {
@@ -63,6 +90,52 @@ namespace WpfApp1
                 }
             }
             mostAwardsWon.Text = mostOscarsTitle;
+        }
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            if (searchFilms.Text.Length == 0)
+            {
+                MessageBox.Show("Kérlek, írd be egy film címét!");
+            }
+            else
+            {
+                    for (int i = 0; i < films.Count(); i++)
+                    {
+                        StringComparison cIgnore = StringComparison.OrdinalIgnoreCase;
+                        if (titles[i].Contains(searchFilms.Text, cIgnore))
+                        {
+                            finds.Text = "A keresett film címe: ";
+                            finds.Text += titles[i];
+                        }
+                    }
+                    if (finds.Text.Length == 0)
+                    {
+                        MessageBox.Show("Sajnos nincs ilyen film a listában");
+                    }
+            }
+        }
+        private void toList_Click(object sender, RoutedEventArgs e)
+        {
+            findsFilms.Items.Clear();
+            if (findsToList.Text.Length == 0)
+            {
+                MessageBox.Show("Kérlek, írd be egy film címét!");
+            }
+            else
+            {
+                for (int i = 0; i < films.Count(); i++)
+                {
+                    StringComparison cIgnore = StringComparison.OrdinalIgnoreCase;
+                    if (titles[i].Contains(findsToList.Text, cIgnore))
+                    {
+                        findsFilms.Items.Add(titles[i]);
+                    }
+                }
+                if (findsFilms.Items.Count == 0)
+                {
+                    MessageBox.Show("Sajnos nincs ilyen film a listában");
+                }
+            }
         }
         public class Films
         {
