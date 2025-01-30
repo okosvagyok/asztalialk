740f109ee74d785b76913f937c140a0e1612a6b0
Rendszergazda <rendszergazda@blathy.info>
2025-01-30 11:27:38 +0100

    to-do list

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,43 +23,69 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
-        private void printText(object sender, SelectionChangedEventArgs args)
+
+        int items = 0;
+        private void addBtn_Click(object sender, RoutedEventArgs e)
         {
-            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
-            if (lbi != null)
+            string newItem = userInput.Text;
+            if (newItem != "")
             {
-                string[] texts = { "Egy - megérett a meggy.", "Kettő - csipkebokor vessző.", "Három - te leszel a párom.", "Négy - bíz oda nem mégy.", "Öt - leszállott a köd." };
-                for (int i = 0; i < texts.Length; i++)
+                items++;
+                todoList.Items.Add(items + ". " + newItem);
+                userInput.Text = "";
+                MessageBox.Show("Tennivaló hozzáadva!");
+            }
+            else
+            {
+                MessageBox.Show("Írj be egy tennivalót!");
+            }
+        }
+        private void userInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (userInput.Text == "Adj meg egy tennivalót!")
+            {
+                userInput.Text = "";
+            }
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (todoList.SelectedIndex != -1)
+            {
+                todoList.Items.RemoveAt(todoList.SelectedIndex);
+                UpdateItemIndixes();
+            }
+            else
+            {
+                MessageBox.Show("Nincs kijelölt elem!");
+            }
+        }
+        private void UpdateItemIndixes()
+        {
+            for (int i = 0; i < todoList.Items.Count; i++)
+            {
+                string item = todoList.Items[i].ToString();
+                int dotIndex = item.IndexOf(". ");
+                if (dotIndex != -1)
                 {
-                    if (lbi.Content.ToString() == (i + 1).ToString())
-                    {
-                        tb.Text = texts[i];
-                    }
+                    todoList.Items[i] = (i + 1) + item.Substring(dotIndex);
                 }
             }
+            items = todoList.Items.Count;
         }
-        private void btn_Click(object sender, RoutedEventArgs e)
+
+        private void deleteAllBtn_Click(object sender, RoutedEventArgs e)
         {
-            lb.Items.Clear();
-            tbx.Text = "Lista kiürítve!";
-            tb.Text = "";
-            btn.Visibility = Visibility.Collapsed;
-        }
-        private void rebtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (lb.Items.Count == 0)
+            if (todoList.Items.Count != 0)
             {
-                lb.Items.Insert(0, "1");
-                lb.Items.Insert(1, "2");
-                lb.Items.Insert(2, "3");
-                lb.Items.Insert(3, "4");
-                lb.Items.Insert(4, "5");
-                lb.FontSize = 30;
-                lb.HorizontalAlignment = HorizontalAlignment.Center;
+                todoList.Items.Clear();
+                items = 0;
+                MessageBox.Show("Minden tennivaló törölve!");
             }
-            tb.Text = "";
-            tbx.Text = "Válassz egy számot";
-            btn.Visibility = Visibility.Visible;
+            else
+            {
+                MessageBox.Show("Nincs mit törölni!");
+            }
         }
     }
 };
